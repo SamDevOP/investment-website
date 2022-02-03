55eeda5aab6ace12c6a55d07765b35cec849b4bf
SamDevOP <samkinyanjui101@gmail.com>
2022-02-03 14:14:05 +0300

    models

--- website_saf/commands.py
+++ website_saf/commands.py
@@ -1,9 +1,6 @@
-import click
-from flask.cli import with_appcontext
+
 
 from models import *
 
-@click.command(name="create_tables")
-@with_appcontext
-def create_tables():
-    db.create_all()
+
+db.create_all()
--- website_saf/create_tables.py
+++ website_saf/create_tables.py
@@ -1,39 +1,46 @@
-# import sqlite3 as sql3
+# # import sqlite3 as sql3
 
-# import pandas as pd 
-# non_normalized_db_filename='peakinvestors'
+# # import pandas as pd 
+# # non_normalized_db_filename='peakinvestors'
 
-# con = sql3.connect(non_normalized_db_filename)
-# cursor = con.cursor()
-# # con.execute("DROP TABLE transactions")
-# # con.execute("CREATE TABLE transactions (email TEXT,type TEXT, Amount INTEGER, invest_date DATETIME,maturity_date DATETIME)")
-# #con.execute("DROP TABLE users")
-# # con.execute("CREATE TABLE users (email TEXT PRIMARY KEY,phone TEXT,passcode VARCHAR NOT NULL,referal_code TEXT)")
-# # con.execute("DROP TABLE fund_transactions")
-# # con.execute("CREATE TABLE fund_transactions (email TEXT,type TEXT, Amount INTEGER, fund_date DATETIME,wallet INT)")
-# # con.execute("DROP TABLE referral")
-# # con.execute("CREATE TABLE referral (email TEXT,refferal_code TEXT,refered_by TEXT,amount_refer_earned INT)")
-# # con.execute("DROP TABLE wallet")
-# # con.execute("CREATE TABLE wallet (email TEXT,wallet_ammount INTEGER);")
-# #con.execute("DROP TABLE investing")
+# # con = sql3.connect(non_normalized_db_filename)
+# # cursor = con.cursor()
+# # # con.execute("DROP TABLE transactions")
+# # # con.execute("CREATE TABLE transactions (email TEXT,type TEXT, Amount INTEGER, invest_date DATETIME,maturity_date DATETIME)")
+# # #con.execute("DROP TABLE users")
+# # # con.execute("CREATE TABLE users (email TEXT PRIMARY KEY,phone TEXT,passcode VARCHAR NOT NULL,referal_code TEXT)")
+# # # con.execute("DROP TABLE fund_transactions")
+# # # con.execute("CREATE TABLE fund_transactions (email TEXT,type TEXT, Amount INTEGER, fund_date DATETIME,wallet INT)")
+# # # con.execute("DROP TABLE referral")
+# # # con.execute("CREATE TABLE referral (email TEXT,refferal_code TEXT,refered_by TEXT,amount_refer_earned INT)")
+# # # con.execute("DROP TABLE wallet")
+# # # con.execute("CREATE TABLE wallet (email TEXT,wallet_ammount INTEGER);")
+# # #con.execute("DROP TABLE investing")
 
-# sql_statement = "select * from users;"
-# df = pd.read_sql_query(sql_statement, con)
-# print(df)
-# con.commit()
-# con.close()
+# # sql_statement = "select * from users;"
+# # df = pd.read_sql_query(sql_statement, con)
+# # print(df)
+# # con.commit()
+# # con.close()
 
-from models import *
+# from models import *
 
 
-mail="rectangularsolutions@protonmail.com"
-# wallet = db.session.query(Wallet).filter(Wallet.email==mail).all()
-# for a in wallet:
-#     print(a.email," wallet ammount is ",int(a.wallet_ammount)+200)
+# mail="rectangularsolutions@protonmail.com"
+# # wallet = db.session.query(Wallet).filter(Wallet.email==mail).all()
+# # for a in wallet:
+# #     print(a.email," wallet ammount is ",int(a.wallet_ammount)+200)
 
-# wallet=db.session.query(Wallet).filter(Wallet.email==mail).first()
-# wallet.wallet_ammount=50000
-active = db.session.query(Activate).filter(Activate.email==mail).first()
-# active.status="DEACTIVATED"
-#active = User.query().first()
-print(active.email)
+# # wallet=db.session.query(Wallet).filter(Wallet.email==mail).first()
+# # wallet.wallet_ammount=50000
+# active = db.session.query(Activate).filter(Activate.email==mail).first()
+# # active.status="DEACTIVATED"
+# #active = User.query().first()
+# print(active.email)
+from flask_sqlalchemy import SQLAlchemy
+
+from app import app 
+
+db = SQLAlchemy(app)
+
+
