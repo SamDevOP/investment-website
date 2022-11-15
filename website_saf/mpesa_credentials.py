799d141650dbf553af4596c42e8ecf80c2ebfb0e
Muhia Kinyanjui <47863714+SamDevOP@users.noreply.github.com>
2022-11-15 18:18:31 +0300

    Update mpesa_credentials.py

--- website_saf/mpesa_credentials.py
+++ website_saf/mpesa_credentials.py
@@ -1,16 +1,6 @@
-CONSUMER_KEY='O8iX4i9GJD2Jky0xckoNJ86t0Dm5njv1'
-CONSUMER_SECRET='MIdpuRk1lvGcsYu9'
+CONSUMER_KEY=''
+CONSUMER_SECRET=''
 
-PASS_KEY='bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+PASS_KEY=''
 
-SECURITY_CREDENTIAL="Jszr21AWP0pn/02kU76ocNwrHJhwIIuCAXDjLpxDZNtQTn/fPzxt1KhjbzipGdxhW/F6zoVWl1/sRdqGy+6l9YXZz8yfMfROmuIIW7KKVzMB17cNYYlnc2yxqNSkf/ink2ctRe47u5yMSMlk2Toncalyg1IIO4i1kKt/3tQtwUSrVayp1vN31+BqeU+CdcYxUuSqMOSS1B03R3s9eDmUQntzg/eUGsfiFsFK9kY1qAX8o3gSqZLQcogv90IgNtjiXsXSw6zJ+bbqIZ+e96V5wjYqfGzO4LnoPv4cnFw/SQj/U02DLASLs+QcOxi55TKNiUlLA8OSsaY1Pth0MrS13g=="
-
-# sum_of_values=0
-# for i in range(0,355):
-#     sum_of_values+=i
-#     if i==20:
-#         print(sum_of_values)
-
-# print("total is ",sum_of_values)
-
-
+SECURITY_CREDENTIAL=""
